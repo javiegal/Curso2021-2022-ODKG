c479406ed48d0069325f71bbdf487037d84cafa4
DAVID LORENZO ALFARO <David.Lorenzo@alu.uclm.es>
2021-10-22 23:03:27 +0200

    Script fixed to clean empty IRIs

--- HandsOn/Group12/mappings/cleaning scripts/remove_empty.py
+++ HandsOn/Group12/mappings/cleaning scripts/remove_empty.py
@@ -1,12 +1,10 @@
+original_filepath = 'data.ttl'
+new_filepath = 'data-cleaned.ttl'
 
-
-original_filepath = 'output.txt'
-new_filepath = 'output_clean.txt'
-
-with open(original_filepath) as original_file:
+with open(original_filepath, encoding='utf-8') as original_file:
    with open(new_filepath, 'w') as new_file:
       line = original_file.readline()
       while line:
-         if '""' not in line:
+         if ('""' not in line) and ('<>' not in line):
             new_file.write(line.strip()+'\n')
          line = original_file.readline()
