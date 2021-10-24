b0ddb9c4d72d8999d5a445760da255e9db39b9fe
CarlosMorote <55406683+CarlosMorote@users.noreply.github.com>
2021-10-24 17:21:02 +0200

    utf-8

--- HandsOn/Group12/mappings/cleaning scripts/remove_empty.py
+++ HandsOn/Group12/mappings/cleaning scripts/remove_empty.py
@@ -1,8 +1,8 @@
-original_filepath = 'data.ttl'
-new_filepath = 'data-cleaned.ttl'
+original_filepath = 'C:/Users/morot/OneDrive - Universidad de Castilla-La Mancha/Master/Open data and graphs/Ejercicios/git/Curso2021-2022-ODKG/HandsOn/Group12/csv/data.ttl'
+new_filepath = 'C:/Users/morot/OneDrive - Universidad de Castilla-La Mancha/Master/Open data and graphs/Ejercicios/git/Curso2021-2022-ODKG/HandsOn/Group12/csv/data-cleaned.ttl'
 
 with open(original_filepath, encoding='utf-8') as original_file:
-   with open(new_filepath, 'w') as new_file:
+   with open(new_filepath, 'w', encoding='utf-8') as new_file:
       line = original_file.readline()
       while line:
          if ('""' not in line) and ('<>' not in line):
--- HandsOn/Group12/rdf/queries.py
+++ HandsOn/Group12/rdf/queries.py
@@ -4,10 +4,10 @@
 from rdflib import Graph, Namespace, Literal
 from rdflib.plugins.sparql import prepareQuery
 
-"""Creamos un grafo vacío"""
+"""Creamos un grafo vacï¿½o"""
 
 g = Graph()
-g.parse("data.ttl", format="ntriples")
+g.parse("C:/Users/morot/OneDrive - Universidad de Castilla-La Mancha/Master/Open data and graphs/Ejercicios/git/Curso2021-2022-ODKG/HandsOn/Group12/csv/data.ttl", format="ntriples")
 
 #for subj, pred, obj in g:
 #  print(subj,pred,obj)
